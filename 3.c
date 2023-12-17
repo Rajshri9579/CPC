dcdd1c71b3f924249a94e6582f3d0d896c6ba5f6
Rajshri Patil <149965316+Rajshri9579@users.noreply.github.com>
2023-12-17 22:15:10 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,36 +1,42 @@
 
 
-#include<stdio.h>
+
+
+#include <stdio.h>
+
 struct books
 {
- char title[50];
- char author[50];
- char subject[100];
- int book_id;
+    char title[50];
+    char author[50];
+    char subject[100];
+    int book_id;
 };
-   int main()
-      {
-             struct books bookArray[5];
-             printf("Enter Details Of Five Books:\n");
-             for(int i=0; i<5; i++)
-             {
-                  printf("Book %d:\n", i+1);
-                  printf("Title:");
-                  scanf("%s", bookArray[i].title);
-                  printf("Author:");
-                  scanf("%s", bookArray[i].author);
-                  printf("Subject:");
-                  scanf("%s", bookArray[i].subject);
-                  printf("Book id:");
-                  scanf("%s",bookArray[i].book_id);
-              }
-     printf("Details of the five books entered by the user:\n");
-for (int i=0 ; i<5 ; i++)
+
+int main()
 {
-     printf("Title:%s\n",bookArray[i].title);
-     printf("Author:%s\n",bookArray[i].author);
-     printf("Subject:%s\n",bookArray[i].subject);
-     printf("Book_id:%d\n",bookArray[i].book_id);
-}
-return 0;
+    struct books bookArray[5];
+    printf("Enter Details Of Five Books:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Book %d:\n", i + 1);
+        printf("Title:");
+        scanf("%s", bookArray[i].title);
+        printf("Author:");
+        scanf("%s", bookArray[i].author);
+        printf("Subject:");
+        scanf("%s", bookArray[i].subject);
+        printf("Book id:");
+        scanf("%d", &bookArray[i].book_id); // Corrected format specifier, added & before the variable
+    }
+
+    printf("Details of the five books entered by the user:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Title:%s\n", bookArray[i].title);
+        printf("Author:%s\n", bookArray[i].author);
+        printf("Subject:%s\n", bookArray[i].subject);
+        printf("Book_id:%d\n", bookArray[i].book_id);
+    }
+
+    return 0;
 }
