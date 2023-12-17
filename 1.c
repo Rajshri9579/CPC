8023d241486024009d849b098f6ce54ccf93bc08
Rajshri Patil <149965316+Rajshri9579@users.noreply.github.com>
2023-12-17 22:08:04 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -5,25 +5,25 @@
 
 void reverseString(char arr[]) {
     int length = strlen(arr);
-    for (int i = 0; i < length / 2; i++) {
-        char temp = arr[i];
-        arr[i] = arr[length - i - 1];
-        arr[length - i - 1] = temp;
+    for (int i = 0; i < length / 2; i++)        {
+    char temp = arr[i];
+    arr[i] = arr[length - i - 1];
+    arr[length - i - 1] = temp;
     }
 }
 
 int main() {
     char arr[10][50];
     for (int i = 0; i < 10; i++) {
-        printf("Enter string no %d: ", i + 1);
-        scanf("%s", arr[i]);
+    printf("Enter string no %d: ", i + 1);
+    scanf("%s", arr[i]);
     }
 
     for (int i = 0; i < 10; i++) {
-        reverseString(arr[i]);
-        printf("Reverse of String %d is: %s\n", i + 1, arr[i]);
+    reverseString(arr[i]);
+    printf("Reverse of String %d is: %s\n", i + 1, arr[i]);
     }
-     return 0;
+    return 0;
 }
 
 
